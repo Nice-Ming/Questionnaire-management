de0f20751e42e858362b9b9eeb682a11cd7887f7
xhwgithub <1565644290@qq.com>
2019-12-29 09:59:45 +0800

    '20200115commit'

---
+++ src/components/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <header id="header">
+    <h2>问卷管理</h2>
+    <router-link tag="h3" to="/">我的问卷</router-link>
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      aray: []
+    };
+  }
+};
+</script>

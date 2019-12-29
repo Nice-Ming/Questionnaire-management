a20606822eba55c22099522fcb5d78b7c3966e9e
xhwgithub <1565644290@qq.com>
2019-12-29 10:14:18 +0800

    部分修改

--- src/components/hello.vue
+++ src/components/hello.vue
@@ -4,17 +4,7 @@
     <router-link tag="h3" to="/">我的问卷</router-link>
   </header>
 </template>
-<<<<<<< HEAD
 
-<script>
-export default {
-    mnethods() {
-      getList() {
-        console.log('helloworld');
-      },
-    },
-}
-=======
 <script>
 export default {
   data() {
@@ -22,6 +12,45 @@ export default {
       aray: []
     };
   }
+
+  mnethods() {
+checkItem(item, flag = null) {
+			if (typeof item.checked === 'undefined') {
+				Vue.set(item, 'checked', true);
+			}
+			else if (flag !== null) {
+				item.checked = !flag;
+			}
+			else {
+				item.checked = !item.checked;
+			}
+		},
+
+		checkAll(flag) {
+			this.quList.forEach( item => {
+				this.checkItem(item, flag)
+			});
+		},
+
+		showPrompt(text) {
+			this.promptText = text;
+			this.isShowPrompt = true;
+		},
+
+    getList() {
+      console.log('helloworld');
+    },
+  },
+
+  watch: {
+		quList: {
+			handler(list) {
+				list.forEach((item, index) => item.id = index + 1);
+				Store.save(list);
+			},
+			deep: true
+		}
+	}
 };
->>>>>>> dev-20200115
+
 </script>
